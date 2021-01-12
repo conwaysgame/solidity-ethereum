d4f5ca6a5e0f95c2f83c530886c0d42c396f1b6b
Dan Hough <daniel.hough@gmail.com>
2021-01-11 17:21:23 -0800

    Updates

--- app.js
+++ app.js
@@ -99,11 +99,11 @@ class App {
 
   showAddress() {
     if (typeof web3 !== 'undefined') {
-      $("#ethAddress").text(process.env.REMOTE_CONTRACT_ADDRESS);
-      $("#ethAddress").attr('title', `Click to send 0.0001ETH to ${process.env.REMOTE_CONTRACT_ADDRESS}`);
-      $("#ethAddress").attr('href', `#`);
-      $("#ethAddress").unbind('click');
-      $("#ethAddress").click(async e => {
+      $(".ethAddress").text(process.env.REMOTE_CONTRACT_ADDRESS);
+      $(".ethAddress, .ethAddressLink").attr('title', `Click to send 0.0001ETH to ${process.env.REMOTE_CONTRACT_ADDRESS}`);
+      $(".ethAddress, .ethAddressLink").attr('href', `#`);
+      $(".ethAddress, .ethAddressLink").unbind('click');
+      $(".ethAddress, .ethAddressLink").click(async e => {
         await this.enableEthereum();
         console.log(`Going to start sending from ${this.currentAccount}`)
         e.preventDefault();
